d8e4fd7f32c6d0af18bfc396c6cab64d987133e0
dwipdarpon20 <dwipdarpon0020@gmail.com>
2026-03-31 01:38:13 +0530

    Chat page basic setup

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,45 @@
+import {create} from 'zustand';
+import {axiosInstance} from '../libs/axios';
+import toast from 'react-hot-toast';
+
+export const useChatStore = create ((set, get)=>({
+    allContacts : [],
+    chats : [],
+    messages : [],
+    activeTab : "chats",
+    selectedUser : null ,
+    isUserLoading : false,
+    isMessagesLoading : false,
+    isSoundEnabled : JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
+
+    toggleSound : ()=> {
+        localStorage.setItem("isSoundEnabled", JSON.stringify(!get().isSoundEnabled));
+        set({isSoundEnabled : !get().isSoundEnabled})
+    },
+
+    setActiveTab : (tab) => set({activeTab : tab}),
+    setSelectedUser : (user) => set({selectedUser : user}),
+    
+    getAllContacts : async () => {
+        set({isUserLoading : true});
+        try {
+            const response = await axiosInstance.get('/messages/contacts');
+            set({allContacts : response.data.contacts, isUserLoading : false});
+        } catch (error) {
+            toast.error("Failed to load contacts");
+            set({isUserLoading : false});
+        }
+    },
+
+    getMyChatPartners : async () => {
+        set({isUserLoading : true});
+        try {
+            const response = await axiosInstance.get('/messages/chats');
+            set({chats : response.data.chats, isUserLoading : false});
+        } catch (error) {
+            toast.error("Failed to load chats");
+            set({isUserLoading : false});
+        }
+    },
+
+}));
