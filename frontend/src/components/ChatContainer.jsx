2e435d108496d0e7b6bba8e8ffbaa881b245c2d6
dwipdarpon20 <dwipdarpon0020@gmail.com>
2026-04-01 01:14:38 +0530

    chat container setup done

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,10 +2,13 @@ import { useEffect } from 'react';
 import { useChatStore } from '../store/useChatStore';
 import { useAuthStore } from '../store/useAuthStore'; 
 import ChatHeader from './ChatHeader';
+import NochatHistoryPlaceholder from './NoChatHistory';
+import MessageInput from './MessageInput';
+import MessageLoadingSkeleton from './MessageLoading';
 
 const ChatContainer = () => {
   const { selectedUser, messages, isMessagesLoading , getMessagesByUserId } = useChatStore();
-  const {authuser} = useAuthStore();
+  const {authUser} = useAuthStore();
   useEffect(() => {
     if (selectedUser) {
       getMessagesByUserId(selectedUser._id);
@@ -14,6 +17,29 @@ const ChatContainer = () => {
   return (
     <>
     <ChatHeader/>
+    <div className='flex-1 px-6 overflow-y-auto py-8'>
+      {messages.length > 0 ? ( 
+        <div className='max-w-3xl mx-auto space-y-6'>
+      {messages.map((msg)=>(
+        <div key = {msg._id}
+        className={`chat ${msg.sender === authUser._id ? 'chat-end' : 'chat-start'}`}
+        >
+          <div className={`chat-bubble ${msg.sender === authUser._id ? 'bg-cyan-500/20 text-cyan-400' : 'bg-slate-700/50 text-slate-200'}`}>
+            {msg.image && (<img src={msg.image} alt="Message" className="max-w-full max-h-full" />)}
+            {msg.text && <p className='mt-2'>{msg.text}</p>}
+            <p className='text-xs mt-1 opacity-70 flex items-center gap-1'>
+              {new Date(msg.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+            </p>
+          </div>
+        </div>
+      ))}
+      </div>
+      
+    ) : isMessagesLoading ? <MessageLoadingSkeleton/> :(
+        <NochatHistoryPlaceholder name = {selectedUser?.fullName || "User"}/>
+      )}
+    </div>
+      <MessageInput/>
     </>
   )
 }
