073c9f05801319b176f66af10de3634252267539
Dwip Darpon Sarker <dwipdarpon0020@gmail.com>
2026-04-02 02:28:08 +0530

    Update axios baseURL for production environment
    Url changed

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create ({
-    baseURL : import.meta.env.MODE === 'production' ? 'https://yourdomain.com/api' : 'http://localhost:3000/api',
+    baseURL : import.meta.env.MODE === 'production' ? 'https://chatapp-backend-y94n.onrender.com' : 'http://localhost:3000/api',
     withCredentials: true,
     
 })
