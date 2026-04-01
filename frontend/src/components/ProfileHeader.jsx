434fd89cbd8f8b3d7a070553df6adc214a347f6a
dwipdarpon20 <dwipdarpon0020@gmail.com>
2026-04-02 03:27:28 +0530

    aww

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -30,7 +30,7 @@ function ProfileHeader() {
   };
 
   return (
-    <div className="p-6 border-b border-slate-700/50">
+    <div className="p-3 border-b border-slate-700/50">
       <div className="flex items-center justify-between">
 
         {/* LEFT */}
