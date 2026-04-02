fb767172df5621aa0a946e232dba4b991e482005
dwipdarpon20 <dwipdarpon0020@gmail.com>
2026-04-02 11:48:02 +0530

    added something in the connectDb

--- backend/src/db/connectDb.js
+++ backend/src/db/connectDb.js
@@ -6,6 +6,7 @@ dotenv.config();
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB successfully ✅');
     } catch (error) {
         console.error('Error connecting to MongoDB: ❌', error);
         process.exit(1);
