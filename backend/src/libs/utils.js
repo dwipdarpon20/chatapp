18839b295ee90a4ba1ef6b70c4676dae2e519c0d
Dwip Darpon Sarker <dwipdarpon0020@gmail.com>
2026-07-02 03:32:07 +0530

    Change SameSite cookie attribute from 'strict' to 'none'

--- backend/src/libs/utils.js
+++ backend/src/libs/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId , res) => {
         res.cookie("jwt" , token , {
         httpOnly : true ,
         secure : process.env.NODE_ENV === "production" ? true : false ,
-        sameSite : "strict",
+        sameSite : "none",
         maxAge : 7 * 24 * 60 * 60 * 1000
     });
     return token ;
