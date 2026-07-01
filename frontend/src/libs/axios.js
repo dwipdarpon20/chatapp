705219a68403ad81024b8cba1103fbce01546591
Dwip Darpon Sarker <dwipdarpon0020@gmail.com>
2026-07-02 04:01:22 +0530

    Fix baseURL for production environment in axios instance

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create ({
-    baseURL : import.meta.env.MODE === 'production' ? 'https://chatapp-backend-j9tx.onrender.com' : 'http://localhost:8080/api',
+    baseURL : import.meta.env.MODE === 'production' ? 'https://chatapp-backend-j9tx.onrender.com/api' : 'http://localhost:8080/api',
     withCredentials: true,
     
 });
